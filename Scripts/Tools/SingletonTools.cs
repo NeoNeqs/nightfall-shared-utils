b6a2a530de8e9d9714fc5eeaae0e9dbf8abea144
Michael March <theneqstech@gmail.com>
2021-04-12 20:53:17 +0200

    Fix merge conflict

---
+++ Scripts/Tools/SingletonTools.cs
@@ -0,0 +1,21 @@
+﻿using SharedUtils.Common;
+
+namespace SharedUtils.Tools
+{
+    public class SingletonTools : GodotSingleton<SingletonTools>
+    {
+        public delegate void ExitTree();
+
+        public event ExitTree ExitTreeSignal;
+
+        SingletonTools()
+        {
+            _singleton = this;
+        }
+
+        public override void _ExitTree()
+        {
+            ExitTreeSignal?.Invoke();
+        }
+    }
+}
